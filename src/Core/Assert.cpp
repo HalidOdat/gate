d9edaec0f80c28d89a2cc8c54d9a6b61965f44f4
Halid Odat <halidodat@gmail.com>
2022-07-20 08:39:56 +0200

    Show string opengl error message

--- src/Core/Assert.cpp
+++ src/Core/Assert.cpp
@@ -11,11 +11,24 @@ namespace Game {
   static_assert(sizeof(GLuint) == sizeof(u32), "GLuints should be unsigned 32-bit numbers");
   static_assert(sizeof(GLint)  == sizeof(i32), "GLuints should be singned 32-bit numbers");
 
+  static const char* glErrorToString(GLenum value) {
+    switch (value) {
+      case GL_NO_ERROR:                      return "no error";
+      case GL_INVALID_ENUM:                  return "invalid enum";
+      case GL_INVALID_VALUE:                 return "invalid value";
+      case GL_INVALID_OPERATION:             return "invalid Operation";
+      case GL_INVALID_FRAMEBUFFER_OPERATION: return "invalid framebuffer operation";
+      case GL_OUT_OF_MEMORY:                 return "out of memory";
+      default: 
+        GAME_UNREACHABLE("unknown OpengGL error type");
+    }
+  }
+
   void CheckOpenGLError(const char* stmt, const char* fname, int line) {
     bool hadError = false;
     GLenum err;
     while ((err = glGetError()) != GL_NO_ERROR) {
-        Logger::error("OpenGL error %08x, at %s:%i - for %s\n", err, fname, line, stmt);
+        Logger::error("OpenGL error 0x%x: %s, at %s:%i - for %s\n", err, glErrorToString(err), fname, line, stmt);
         hadError = true;
     }
     if (hadError) {
