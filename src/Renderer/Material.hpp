2af5a95727b79db2228a85952b0930bd4d31604b
Halid Odat <halidodat@gmail.com>
2022-08-03 15:17:47 +0200

    Add alpha mode

--- src/Renderer/Material.hpp
+++ src/Renderer/Material.hpp
@@ -7,23 +7,31 @@ namespace Game {
   
   class Material {
   public:
+    enum class AlphaMode {
+      Opaque,
+    };
+
+  public:
     Material() {}
 
     inline void setDiffuseMap(const Texture2D& texture) {  mDiffuseMap = texture; }
     inline void setSpecularMap(const Texture2D& texture) { mSpecularMap = texture; }
     inline void setEmissionMap(const Texture2D& texture) { mEmissionMap = texture; }
     inline void setShininess(f32 value) { mShininess = value; }
+    inline void setAlphaMode(AlphaMode mode) { mAlphaMode = mode; }
 
     inline const Texture2D& getDiffuseMap()  const { return mDiffuseMap; }
     inline const Texture2D& getSpecularMap() const { return mSpecularMap; }
     inline const Texture2D& getEmissionMap() const { return mEmissionMap; }
     inline f32 getShininess() const { return mShininess; }
+    inline AlphaMode getAlphaMode() const { return mAlphaMode; }
 
   private:
     Texture2D mDiffuseMap;
     Texture2D mSpecularMap;
     Texture2D mEmissionMap;
     f32       mShininess;
+    AlphaMode mAlphaMode = AlphaMode::Opaque;
 
     friend class Renderer;
   };
