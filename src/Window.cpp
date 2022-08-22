28acb751237fc8c3e2590dcc09ac71f2c96a88f8
HalidOdat <halidodat@gmail.com>
2022-08-22 12:40:56 +0200

    Setup OpenGL Debug Context

--- src/Window.cpp
+++ src/Window.cpp
@@ -47,6 +47,63 @@ namespace Game {
     }    
   }
 
+  void APIENTRY openGLMessageCallback(
+    GLenum source,
+    GLenum type,
+    GLuint id,
+    GLenum severity,
+    GLsizei length,
+    const GLchar* message,
+    const void* userParam
+  ) {
+    (void)id;
+    (void)length;
+    (void)userParam;
+
+    const char* sourceString;
+    switch (source) {
+      case GL_DEBUG_SOURCE_API:             sourceString = "API"; break;
+      case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   sourceString = "Window System"; break;
+      case GL_DEBUG_SOURCE_SHADER_COMPILER: sourceString = "Shader Compiler"; break;
+      case GL_DEBUG_SOURCE_THIRD_PARTY:     sourceString = "Third Party"; break;
+      case GL_DEBUG_SOURCE_APPLICATION:     sourceString = "Application"; break;
+      case GL_DEBUG_SOURCE_OTHER:           sourceString = "Other"; break;
+      default:
+        GAME_UNREACHABLE("");
+    }
+
+    const char* typeString;
+    switch (type) {
+      case GL_DEBUG_TYPE_ERROR:               typeString = "Error"; break;
+      case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: typeString = "Deprecated Behaviour"; break;
+      case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  typeString = "Undefined Behaviour"; break;
+      case GL_DEBUG_TYPE_PORTABILITY:         typeString = "Portability"; break;
+      case GL_DEBUG_TYPE_PERFORMANCE:         typeString = "Performance"; break;
+      case GL_DEBUG_TYPE_MARKER:              typeString = "Marker"; break;
+      case GL_DEBUG_TYPE_PUSH_GROUP:          typeString = "Push Group"; break;
+      case GL_DEBUG_TYPE_POP_GROUP:           typeString = "Pop Group"; break;
+      case GL_DEBUG_TYPE_OTHER:               typeString = "Other"; break;
+      default:
+        GAME_UNREACHABLE("");
+    }
+    
+    switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH:
+          Logger::error("OpenGL: Source = %s, Type = %s, Message = %s", sourceString, typeString, message);
+          break;
+        case GL_DEBUG_SEVERITY_MEDIUM:
+          Logger::warn("OpenGL: Source = %s, Type = %s, Message = %s", sourceString, typeString, message);
+          break;
+        case GL_DEBUG_SEVERITY_LOW:
+          Logger::info("OpenGL: Source = %s, Type = %s, Message = %s", sourceString, typeString, message);
+          break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+          Logger::trace("OpenGL: Source = %s, Type = %s, Message = %s", sourceString, typeString, message);
+          break;
+        default:
+          GAME_UNREACHABLE("");
+    }
+  }
 
   Ref<Window> Window::create(const char* title, u32 width, u32 height) {
     GAME_PROFILE_FUNCTION();
@@ -54,13 +111,17 @@ namespace Game {
 
     Logger::trace("Creating window...");
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_SAMPLES, 4);
 
     #ifdef __APPLE__
       glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
+    #if GAME_DEBUG_MODE && !defined(GAME_PLATFORM_WEB)
+      glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
     #endif
 
     Data data;
@@ -86,7 +147,20 @@ namespace Game {
 
     Logger::info("OpenGL Version: %s", glGetString(GL_VERSION));
 
-    // glfwSwapInterval(1);
+    #if GAME_DEBUG_MODE && !defined(GAME_PLATFORM_WEB)
+      i32 flags;
+      glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+      if (flags & GL_CONTEXT_FLAG_DEBUG_BIT) {
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        glDebugMessageCallback(openGLMessageCallback, nullptr);
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+
+        Logger::info("Glfw: Debug context initialized!");
+      } else {
+        Logger::warn("Glfw: Failed to initialize the debug context");
+      }
+    #endif
 
     glfwSetWindowUserPointer(result->data.window, &result->data);
 
