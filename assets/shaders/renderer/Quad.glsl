d19e50f7590dd0036add1baccd010452f9af0615
Halid Odat <halidodat@gmail.com>
2023-02-02 22:35:57 +0100

    Deploy in release mode

--- assets/shaders/renderer/Quad.glsl
+++ assets/shaders/renderer/Quad.glsl
@@ -28,6 +28,10 @@ flat in uint vTexIndex;
 
 uniform sampler2D uTextures[16];
 
+// uniform float uTime;
+// uniform vec2  uResolution;
+// uniform float uYes;
+
 void main() {
    // Reason for the switch cases: 
    // https://stackoverflow.com/questions/72648980/opengl-sampler2d-array
@@ -50,5 +54,22 @@ void main() {
       case 14u: color = texture(uTextures[14], vTexCoord); break;
       case 15u: color = texture(uTextures[15], vTexCoord); break;
    }
-   FragColor = color * vColor;
+   // if (uYes >= 1.0f) {
+   //    vec2 frag_uv = gl_FragCoord.xy / uResolution;
+   //    vec3 color_gap = vec3(0.25);
+   //    // vec3 color_stripe = vec3(1.0, 0.75, 0.0);
+   //    vec3 color_stripe = vec3(1.0, 0.20, 0.0);
+   //    float divisions = 12.0; // increase for more stripe density
+   //    float stripe_bias = 2.0; // 1.0 means no stripes; 2.0 means stripes and gaps are equal size
+   //    float speed = 0.1;
+   //    float angle = 0.7854; // in radians
+   //    float w = cos(angle) * frag_uv.x + sin(angle) * frag_uv.y - speed * uTime;
+	//    if (floor(mod(w * divisions, stripe_bias)) < 0.0001) {
+	// 	   FragColor = vec4(color_gap, 1.0f);
+	//    } else {
+	// 	   FragColor = vec4(color_stripe, 1.0f);
+	//    }
+   // } else {
+      FragColor = color * vColor;
+   // }
 }
